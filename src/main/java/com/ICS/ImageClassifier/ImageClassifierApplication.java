32d864ec98da3937248d46879f3ba0a8dcbb8862
MariaIvanova01 <Maria.K.Ivanova01@gmail.com>
2023-05-02 00:40:29 +0300

    Initial commit

---
+++ src/main/java/com/ICS/ImageClassifier/ImageClassifierApplication.java
@@ -0,0 +1,13 @@
+package com.ICS.ImageClassifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageClassifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageClassifierApplication.class, args);
+	}
+
+}
