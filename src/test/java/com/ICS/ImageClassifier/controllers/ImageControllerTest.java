b76f7ef69eed5342395e077cd56692f5e8752fdf
MariaIvanova01 <Maria.K.Ivanova01@gmail.com>
2023-05-10 13:37:43 +0300

    Added unit test for createImage happy scenario

--- src/test/java/com/ICS/ImageClassifier/controllers/ImageControllerTest.java
+++ src/test/java/com/ICS/ImageClassifier/controllers/ImageControllerTest.java
@@ -1,8 +1,39 @@
 package com.ICS.ImageClassifier.controllers;
 
+import com.ICS.ImageClassifier.models.Image;
+import com.ICS.ImageClassifier.services.ImageClassificationWrapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(controllers = ImageController.class)
 public class ImageControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ImageClassificationWrapper imageClassificationWrapper;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Test
+    public void createImageOnPostPositive() throws Exception {
+        String url = "https://travelsteps.net/uploads/more-prez-septemvri.jpg";
+
+        mockMvc.perform(post("/rest/getImageURL")
+                .contentType("application/json")
+                .content(url))
+                .andExpect(status().isOk());
+    }
+
 
 
 }
